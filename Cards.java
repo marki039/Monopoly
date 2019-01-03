fd41be410b12ab333ba88d08bc4a088ca3527f24
MIchael Markiewicz <43661106+marki039@users.noreply.github.com>
2019-01-03 11:33:24 -0600

    Create Cards.java
    Added chance and community cards

---
+++ Cards.java
@@ -0,0 +1,48 @@
+// Creation Date: 01/03/2019
+// Creator: Michael Markiewicz
+
+public class Cards
+{
+    public final static String [] communityChest =
+    {
+        "Advance to Go (Collect $200)",                                                 // 0
+        "Bank error in your favor – collect $75",                                       // 1
+        "Doctor's fees – Pay $50",                                                      // 2
+        "Get out of jail free – this card may be kept until needed, or sold",           // 3
+        "Go to jail – go directly to jail – Do not pass Go, do not collect $200",       // 4
+        "It is your birthday Collect $10 from each player",                             // 5
+        "Grand Opera Night – collect $50 from every player for opening night seats",    // 6
+        "Income Tax refund – collect $20",                                              // 7
+        "Life Insurance Matures – collect $100",                                        // 8
+        "Pay Hospital Fees of $100",                                                    // 9
+        "Pay School Fees of $50",                                                       // 10
+        "Receive $25 Consultancy Fee",                                                  // 11
+        "You are assessed for street repairs – $40 per house, $115 per hotel",          // 12
+        "You have won second prize in a beauty contest– collect $10",                   // 13
+        "You inherit $100",                                                             // 14
+        "From sale of stock you get $50",                                               // 15
+        "Holiday Fund matures - Receive $100"                                           // 16
+    }
+
+    public final static String [] chanceCards =
+    {
+        "Advance to Go (Collect $200)",                                                                                                                                         // 0
+        "Advance to Illinois Ave.",                                                                                                                                             // 1
+        "Advance token to nearest Utility. If unowned, you may buy it from the Bank. If owned, throw dice and pay owner a total ten times the amount thrown.",                  // 2
+        "Advance token to the nearest Railroad and pay owner twice the rental to which he/she is otherwise entitled. If Railroad is unowned, you may buy it from the Bank.",    // 3
+        "Advance token to the nearest Railroad and pay owner twice the rental to which he/she is otherwise entitled. If Railroad is unowned, you may buy it from the Bank.",    // 4
+        "Advance to St. Charles Place – if you pass Go, collect $200",                                                                                                          // 5
+        "Bank pays you dividend of $50",                                                                                                                                        // 6
+        "Get out of Jail free – this card may be kept until needed, or traded/sold",                                                                                            // 7
+        "Go back 3 spaces",                                                                                                                                                     // 8
+        "Go directly to Jail – do not pass Go, do not collect $200",                                                                                                            // 9
+        "Make general repairs on all your property – for each house pay $25 – for each hotel $100",                                                                             // 10
+        "Pay poor tax of $15",                                                                                                                                                  // 11
+        "Take a trip to Reading Railroad – if you pass Go collect $200",                                                                                                        // 12
+        "Take a walk on the Boardwalk – advance token to Boardwalk",                                                                                                            // 13
+        "You have been elected chairman of the board – pay each player $50",                                                                                                    // 14
+        "Your building loan matures – collect $150",                                                                                                                            // 15
+        "You have won a crossword competition - collect $100"                                                                                                                   // 16
+    }
+
+}
