3cbdff266f9851bcf1edf50b69d7aefcaa07ff42
MIchael Markiewicz <43661106+marki039@users.noreply.github.com>
2019-01-03 00:30:02 -0600

    Implemented Base Color Grouping and various special tiles
    Still need to impliment houses/hotels, as well as morgages, auctioning, chance and community cards, human players, ect...

---
+++ Improvement.java
@@ -0,0 +1,30 @@
+// Creation Date: 01/02/2019
+// Creator: Michael Markiewicz
+
+class Improvement
+{
+    public final static int [] P =      // purple group
+    {1, 3};
+    public final static Integer [] ownsP = new Integer[2];   // lists players that own a plot in P
+    public final static int [] LB =     // light blue group
+    {6, 8, 9};
+    public final static Integer [] ownsLB = new Integer[3];  // lists players that own a plot in LB
+    public final static int [] M =      // maroon group
+    {11, 13, 14};
+    public final static Integer [] ownsM = new Integer[3];   // lists players that own a plot in M
+    public final static int [] O =      // orange group
+    {16, 18, 19};
+    public final static Integer [] ownsO = new Integer[3];   // lists players that own a plot in O
+    public final static int [] R =      // red group
+    {21, 23, 24};
+    public final static Integer [] ownsR = new Integer[3];   // lists players that own a plot in R
+    public final static int [] Y =      // yellow group
+    {26, 27, 29};
+    public final static Integer [] ownsY = new Integer[3];   // lists players that own a plot in Y
+    public final static int [] G =      // green group
+    {31, 32, 34};
+    public final static Integer [] ownsG = new Integer[3];  // lists players that own a plot in G
+    public final static int [] DB =     // dark blue group
+    {37, 39};
+    public final static Integer [] ownsDB = new Integer[2];  // lists players that own a plot in DB
+}
